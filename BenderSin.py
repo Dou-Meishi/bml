57e90a1d813fad789c372b7f29c788d2b6fb3307
Dou Meishi <doumeishid@gmail.com>
2023-03-09 21:56:27 +0800

    change search region

--- BenderSin.py
+++ BenderSin.py
@@ -253,9 +253,9 @@ def solve_BenderSin(n, *, dirac, repeat=10, **solver_kws):
 # +
 search_mesh = {
     'dirac': [False], #, True],
-    'y_lr': [0.05, 0.01, 5e-3], #, 5e-4, 5e-5],
-    'z_lr': [0.01, 5e-3, 1e-3],
-    'batch_size': [512], #, 1024],
+    'y_lr': [0.01], #, 5e-4, 5e-5],
+    'z_lr': [7e-3, 2e-3],
+    'batch_size': [512, 1024], #, 1024],
 }
 
 res = []
