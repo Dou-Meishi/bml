3251cf786b6ca3d7070ec884d68b9dbddace5f4b
Dou Meishi <doumeishid@gmail.com>
2023-03-17 17:31:06 +0800

    Add _Fixt0 nets in yznets.py

--- yznets.py
+++ yznets.py
@@ -2,6 +2,8 @@
 
 import torch
 
+# ## Simple FC Nets
+
 # +
 class YNet_FC2L(torch.nn.Module):
     
@@ -75,3 +77,52 @@ class ZNet_FC3L(torch.nn.Module):
     def forward(self, t, x):
         z = torch.cat([t, x], dim=-1)
         return self.fcnet(z).view(*x.shape[:-1], self.m, self.d)
+# -
+
+# ## FC Nets with Fixed $y_0$
+
+class YNet_FC2L_Fixt0(torch.nn.Module):
+    
+    def __init__(self, n, m, *, hidden_size):
+        super().__init__()
+        
+        self.register_parameter(
+            'y0',
+            torch.nn.Parameter(torch.rand(m).uniform_(1, 2))
+        )
+
+        self.fcnet = torch.nn.Sequential(
+            torch.nn.Linear(1+n, hidden_size, bias=True),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_size, m, bias=True)
+        )
+        
+    def forward(self, t, x):
+        chi = (abs(t)>1e-15).float().to(device=x.device, dtype=x.dtype)
+        z = torch.cat([t, x], dim=-1)
+        return self.fcnet(z)*chi + self.y0
+
+
+class YNet_FC3L_Fixt0(torch.nn.Module):
+    
+    def __init__(self, n, m, *, hidden_size):
+        super().__init__()
+        
+        self.register_parameter(
+            'y0',
+            torch.nn.Parameter(torch.rand(m).uniform_(1, 2))
+        )
+        
+        self.fcnet = torch.nn.Sequential(
+            torch.nn.Linear(1+n, hidden_size, bias=True),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_size, hidden_size, bias=True),
+            torch.nn.ReLU(),
+            torch.nn.Linear(hidden_size, m, bias=True)
+        )
+        
+    def forward(self, t, x):
+        chi = (abs(t)>1e-15).float().to(device=x.device, dtype=x.dtype)
+        z = torch.cat([t, x], dim=-1)
+        return self.fcnet(z)*chi + self.y0
+
